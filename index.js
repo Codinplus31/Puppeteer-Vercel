a8f15df95019cadae974bdd3ea3b7613e1f13a93
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2024-05-17 17:46:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,26 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-ngrok.listen(app).then(() => {
+/*ngrok.listen(app).then(() => {
   console.log("established listener at: " + app.listener.url());
-});
+});*/
+async function setup() {
+  // create session
+  const session = await new ngrok.SessionBuilder()
+    .authtokenFromEnv()
+    .metadata("Online in One Line")
+    .connect();
+  // create listener
+  const listener = await session
+    .httpEndpoint()
+    .allowCidr("0.0.0.0/0")
+    .oauth("google")
+    .requestHeader("X-Req-Yup", "true")
+    .listen();
+  // link listener to app
+  const socket = await ngrok.listen(app, listener);
+  console.log(`Ingress established at: ${listener.url()}`);
+  console.log(`Express listening on: ${socket.address()}`);
+}
+
+setup()
