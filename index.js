dd3786e3c5d8ae12247806dcfc42c8284d689999
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2024-05-17 16:37:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,58 +1,11 @@
-const app = require("express")();
-const puppeteer = require("puppeteer");
+const express = require("express");
+const ngrok = require("@ngrok/ngrok");
+const app = express();
 
-require("dotenv").config();
-  
-console.log(process.env.NODE_ENV,"node_env")
-console.log(process.env.PUPPETEER_EXECUTABLE_PATH,"node_exec")
-app.get("/api", async (req, res) => {
-  let options = {};
-
- /* if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-    options = {
-     // args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
-     args: [
-      "--disable-setuid-sandbox",
-      "--no-sandbox",
-      "--single-process",
-      "--no-zygote",
-    ],
-      defaultViewport: chrome.defaultViewport,
-      executablePath: await chrome.executablePath,
-      headless: true,
-      ignoreDefaultArgs: ['--disable-extensions'],
-      
-      ignoreHTTPSErrors: true,
-    };
-    console.log("core working")
-  }
-*/
-  try {
-  //  let browser = await puppeteer.launch(options);
-const browser = await puppeteer.launch({
-    args: [
-      "--disable-setuid-sandbox",
-      "--no-sandbox",
-      "--single-process",
-      "--no-zygote",
-    ],
-    executablePath:
-      process.env.NODE_ENV === "production"
-        ? process.env.PUPPETEER_EXECUTABLE_PATH
-        : puppeteer.executablePath(),
-    
-  });
-    let page = await browser.newPage();
-    await page.goto("https://watchoutmovies.vercel.app/");
-    res.send(await page.title());
-  } catch (err) {
-    console.error(err);
-    return null;
-  }
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.listen(process.env.PORT, () => {
-  console.log("Server started");
+ngrok.listen(app).then(() => {
+  console.log("established listener at: " + app.listener.url());
 });
-
-module.exports = app;
